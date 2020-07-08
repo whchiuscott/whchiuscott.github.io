fbd3b48a53e9a6c3fd162c30330f970fa018085f
whchiuscott <67994220+whchiuscott@users.noreply.github.com>
2020-07-08 13:19:43 +0800

    Add files via upload

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,11 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let myVariable = document.querySelector('h1');
+
+alert('hello!');
+
+function multiply(2,3) {
+  let result = 2 * 3;
+  return result;
+}
